a3243721ad43d053a1e5cd9153f41b5ad511f279
kuper0201 <kuper171@gmail.com>
2025-02-01 15:06:40 +0900

    update no_turbo/no_thunderbolt

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo pacman -Syu pulseaudio
-
 # screen backlight
 sudo mv 99-backlight.rules /etc/udev/rules.d/99-backlight.rules
 
@@ -9,7 +7,17 @@ sudo mv 99-backlight.rules /etc/udev/rules.d/99-backlight.rules
 sudo mv 90-keyboard-backlight.rules /etc/udev/rules.d/90-keyboard-backlight.rules
 
 # touchpad drag lock
-sudo mv 40-libinput.conf /etc/X11/xorg.conf.d/40-libinput.conf
+# sudo mv 40-libinput.conf /etc/X11/xorg.conf.d/40-libinput.conf
+
+# no intel turbo boost
+sudo mv intel-noturbo.service /etc/systemd/system/intel-noturbo.service
+sudo mkdir -p /opt/intel
+sudo mv intel_noturbo.sh /opt/intel/intel_noturbo.sh
+sudo chmod +x /opt/intel/intel_noturbo.sh
+sudo systemctl enable intel-noturbo
+
+# off thunderbolt
+sudo sed -i 's/ext4/ext4 quiet splash acpi_osi=!Darwin/g' /boot/loader/entries/*_linux-zen.conf
 
 # reload rules(or reboot manually)
 sudo udevadm control --reload-rules
